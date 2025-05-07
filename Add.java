f3f221b88182285add2d75665acb6c6702ddb525
01fe23bca055 <01fe23bca055@kletech.ac.in>
2025-05-06 23:09:32 -0700

    devops

---
+++ Add.java
@@ -0,0 +1,14 @@
+
+public class Add{
+    public static void main(String[] args) {
+        // Declare and initialize two numbers
+        int num1 = 15;
+        int num2 = 25;
+
+        // Calculate the sum
+        int sum = num1 + num2;
+
+        // Display the result
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+    }
+}
